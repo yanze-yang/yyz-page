6ea36b283c111351e77cb603203851184c2c004d
Ryan Yang <ryanyang.nz@gmail.com>
2022-12-05 20:31:57 +1300

    add Google Font

--- _document.tsx
+++ _document.tsx
@@ -1,5 +1,13 @@
-import Document, { DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -21,5 +29,26 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google Fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Source+Sans+Pro:ital@0;1&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
